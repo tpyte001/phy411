6923453f613895995d2fd1ea90ad2ca5e091ed91
tpyte001 <tpyte001@plattsburgh.edu>
2022-09-10 17:30:01 -0400

    started hw

--- fib_num.py
+++ fib_num.py
@@ -2,4 +2,10 @@ import numpy
 
 #print out n = 10,000 fibonacci number
 #maybe draw with turtle 
-# F_{n} = F_{n-1} + F_{n+1}
+# F_{n} = F_{n-2} + F_{n-1}
+
+
+F = numpy.array([i for i in range(1,10)])
+print(f)
+
+
