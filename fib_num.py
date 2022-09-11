2726a5846cac8ebf826deddd7050800a65ce3f06
tpyte001 <tpyte001@plattsburgh.edu>
2022-09-10 20:26:57 -0400

    Finished HW

--- fib_num.py
+++ fib_num.py
@@ -1,12 +1,36 @@
 # homework 1 phy411
 import numpy
 
-#print out n = 10,000 fibonacci number
 #maybe draw with turtle 
 # F_{n} = F_{n-2} + F_{n-1}
 
 
-F = numpy.array([i for i in range(1,10)])
-print(f)
+
+# gives you specific Fibonocci number
+
+def fib_number(n):
+    n = (n-2)+(n-1)
+    print(n)
+
+fib_number(10000)
+
+# generates Fibonocci sequence
+
+def fib_seq(n):
+    fib = []
+    for i in range(n):
+        a = (i-1)
+        b = (i-2)
+        c = a + b
+        fib.append(c)
+    print(fib)
+    
+
+n = 100
+
+if fib_number(n) == fib_seq(n):
+    print("You can go to bed now.")
+else:
+    print("Keep trying.")
 
 
