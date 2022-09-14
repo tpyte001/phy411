c3e1b19222acf3f2b5e915b74468a73c0e9eb8d3
Taylor Pytel <90528316+tpyte001@users.noreply.github.com>
2022-09-14 14:11:56 -0400

    class

---
+++ E_field.py
@@ -0,0 +1,26 @@
+#xlist, ylist array from -10,10, 0.01
+import numpy as np
+import matplotlib.pyplot as mpl
+
+xlist = np.arange(-5,5,0.1)
+ylist = np.arange(-5,5,0.1)
+i = 0
+q = 1
+k = 1
+Ex = np.zeros((100,100)) #len(xlist)
+Ey = np.zeros((100,100)) #len(ylist)
+for x in xlist:
+    j = 0
+    for y in ylist:   
+        Ex[j,i] = x/(np.sqrt(x**2+y**2)**3) # add x comp of other charge
+        Ey[j,i] = y/(np.sqrt(x**2+y**2)**3) # to y comp of everything 
+        j = j+1
+    i = i+1
+mpl.streamplot(xlist,ylist,Ex,Ey)
+#mpl.streamplot(xlist,ylist,Ey,Ex)
+mpl.show()
+# plt.quiver(xlist,ylist,Ey,Ex)
+# Homework make for 2 point charges
+# q = 1 at (1,0)
+# q = 1 at (-1,0)
+# review electric field charges 
